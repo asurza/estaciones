61efeb9663cc78ab3b4b460e00a14d43e08a8bee
Enrique <enriqueasurza@gmail.com>
2024-05-05 13:27:22 +0200

    Fix dependencias

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,11 @@ import App from './App.jsx'
 import './index.css'
 
 import {NextUIProvider} from "@nextui-org/react";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <NextUIProvider>
     <main className="light text-foreground bg-background">
-      <DevSupport ComponentPreviews={ComponentPreviews}
-                  useInitialHook={useInitial}
-      >
         <App/>
-      </DevSupport>
     </main>
   </NextUIProvider>
 )
